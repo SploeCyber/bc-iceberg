ebb0bbb4558f83cf2259cf564945c330167b9e4e
SploeCyber <champgamerthai@gmail.com>
2023-08-06 14:31:03 +0700

    hope it work

--- script.js
+++ script.js
@@ -1,101 +1,113 @@
-async function fetchData() {
-    try {
-        const response = await fetch('entries.json');
-        const data = await response.json();
-        return data;
-    } catch (error) {
+(async function () {
+  const tabsContainer = document.getElementById('tabsContainer');
+
+  function fetchData() {
+    return fetch('entries.json')
+      .then(response => response.json())
+      .catch(error => {
         console.error('Error fetching data:', error);
         return [];
-    }
-}
+      });
+  }
 
-function displayData(entries) {
-    entries.forEach((entry) => {
-        const entryDiv = document.createElement('div');
-        entryDiv.classList.add('tab-content');
+  function displayData(entries) {
+    const allTags = new Set();
+    const entryElements = [];
 
-        const title = document.createElement('h2');
-        title.innerHTML = entry.name;
-        entryDiv.appendChild(title);
+    entries.forEach(entry => {
+      const entryDiv = document.createElement('div');
+      entryDiv.classList.add('tab-content');
+      entryDiv.dataset.tier = entry.tier || 'N/A';
 
-        if (entry.description) {
-            const description = document.createElement('p');
-            description.innerHTML = entry.description;
-            entryDiv.appendChild(description);
-        }
+      const title = document.createElement('h2');
+      title.innerHTML = entry.name;
+      entryDiv.appendChild(title);
 
-        if (entry.media && entry.mediaType === 'image' && entry.media !== 'none') {
-            const media = document.createElement('img');
-            media.setAttribute('src', `media/${entry.media}`);
-            media.setAttribute('alt', entry.name);
-            entryDiv.appendChild(media);
-        }
+      if (entry.description) {
+        const description = document.createElement('p');
+        description.innerHTML = entry.description;
+        entryDiv.appendChild(description);
+      }
 
-        const tierTag = document.createElement('p');
-        tierTag.textContent = `Tier: ${entry.tier || 'N/A'}`;
-        entryDiv.appendChild(tierTag);
+      if (entry.media && entry.mediaType === 'image' && entry.media !== 'none') {
+        const media = document.createElement('img');
+        media.setAttribute('src', `media/${entry.media}`);
+        media.setAttribute('alt', entry.name);
+        entryDiv.appendChild(media);
+      }
 
-        const tags = document.createElement('p');
-        tags.classList.add('tags');
-        tags.textContent = `Tags: ${entry.tags ? entry.tags.join(', ') : 'N/A'}`;
-        entryDiv.appendChild(tags);
+      const tierTag = document.createElement('p');
+      tierTag.textContent = `Tier: ${entry.tier || 'N/A'}`;
+      entryDiv.appendChild(tierTag);
 
-        const tierTab = document.getElementById(`tier${entry.tier}`);
-        if (tierTab) {
-            tierTab.appendChild(entryDiv);
-        }
+      const tags = document.createElement('p');
+      tags.classList.add('tags');
+      if (entry.tags) {
+        entry.tags.forEach(tag => allTags.add(tag));
+        tags.innerHTML = `Tags: ${entry.tags.map(tag => `<span class="tag" data-tag="${tag}">${tag}</span>`).join(', ')}`;
+      } else {
+        tags.textContent = 'Tags: N/A';
+      }
+      entryDiv.appendChild(tags);
+
+      entryElements.push(entryDiv);
     });
-}
 
-function findUniqueTiers(entries) {
-    const uniqueTiers = new Set();
-    entries.forEach(entry => uniqueTiers.add(entry.tier));
-    return Array.from(uniqueTiers).sort();
-}
+    tabsContainer.innerHTML = '';
 
-function createTabs(uniqueTiers) {
-    const tabsContainer = document.getElementById('tabsContainer');
-
+    const uniqueTiers = findUniqueTiers(entries);
     uniqueTiers.forEach(tier => {
-        const tabLink = document.createElement('div');
-        tabLink.classList.add('tab-link');
-        tabLink.textContent = `Tier ${tier}`;
-        tabLink.setAttribute('data-tab', `tier${tier}`);
-        tabsContainer.appendChild(tabLink);
+      const tabLink = document.createElement('div');
+      tabLink.classList.add('tab-link');
+      tabLink.textContent = `Tier ${tier}`;
+      tabLink.setAttribute('data-tab', `tier${tier}`);
+      tabsContainer.appendChild(tabLink);
 
-        const tabContent = document.createElement('div');
-        tabContent.classList.add('tab');
-        tabContent.setAttribute('id', `tier${tier}`);
-        document.body.appendChild(tabContent);
+      const tabContent = document.createElement('div');
+      tabContent.classList.add('tab');
+      tabContent.setAttribute('id', `tier${tier}`);
+      document.body.appendChild(tabContent);
     });
-}
 
-function switchTab(tabName) {
-    const tabs = document.querySelectorAll('.tab');
-    tabs.forEach(tab => tab.classList.remove('active'));
+    const tagsContainer = document.createElement('div');
+    tagsContainer.classList.add('tags-container');
+    allTags.forEach(tag => {
+      const tagElement = document.createElement('span');
+      tagElement.classList.add('tag');
+      tagElement.textContent = tag;
+      tagElement.setAttribute('data-tag', tag);
+      tagsContainer.appendChild(tagElement);
+    });
+    document.body.appendChild(tagsContainer);
 
-    const tabLinks = document.querySelectorAll('.tab-link');
-    tabLinks.forEach(tabLink => tabLink.classList.remove('active'));
-
-    const selectedTab = document.getElementById(tabName);
-    selectedTab.classList.add('active');
-
-    const selectedTabLink = document.querySelector(`[data-tab="${tabName}"]`);
-    selectedTabLink.classList.add('active');
-}
-
-window.onload = async () => {
-    const data = await fetchData();
-    const uniqueTiers = findUniqueTiers(data);
-    createTabs(uniqueTiers);
-
-    displayData(data);
+    entryElements.forEach(entryElement => {
+      const tierTab = document.getElementById(`tier${entryElement.dataset.tier}`);
+      if (tierTab) {
+        tierTab.appendChild(entryElement);
+      }
+    });
 
     const tabLinks = document.querySelectorAll('.tab-link');
     tabLinks.forEach(tabLink => {
-        tabLink.addEventListener('click', () => {
-            const tabName = tabLink.getAttribute('data-tab');
-            switchTab(tabName);
-        });
+      tabLink.addEventListener('click', () => {
+        const tabName = tabLink.getAttribute('data-tab');
+        switchTab(tabName);
+      });
     });
-};
+
+    const tagElements = document.querySelectorAll('.tag');
+    tagElements.forEach(tagElement => {
+      tagElement.addEventListener('click', () => {
+        const selectedTag = tagElement.getAttribute('data-tag');
+        filterByTag(selectedTag);
+      });
+    });
+  }
+
+  function findUniqueTiers(entries) {
+    const uniqueTiers = new Set();
+    entries.forEach(entry => uniqueTiers.add(entry.tier));
+    return Array.from(uniqueTiers).sort();
+  }
+
+ 
