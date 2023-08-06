5cd609be375d1df8f6edf1e282e13da0b99a517f
SploeCyber <champgamerthai@gmail.com>
2023-08-06 15:13:19 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -34,12 +34,25 @@ function createEntryDiv(entry) {
   entryDiv.appendChild(title);
 
   const descriptionElement = document.createElement('p');
-  descriptionElement.innerHTML = entry.description;
+  const maxDescriptionLength = 100; // Set your desired maximum length
+  if (entry.description.length > maxDescriptionLength) {
+    descriptionElement.innerHTML = entry.description.substring(0, maxDescriptionLength) + '...';
+    const readMoreLink = document.createElement('a');
+    readMoreLink.href = '#';
+    readMoreLink.textContent = 'Read More';
+    readMoreLink.addEventListener('click', (event) => {
+      event.preventDefault();
+      descriptionElement.innerHTML = entry.description;
+    });
+    entryDiv.appendChild(readMoreLink);
+  } else {
+    descriptionElement.innerHTML = entry.description;
+  }
   entryDiv.appendChild(descriptionElement);
 
   if (entry.media && entry.mediaType === 'image') {
     const media = document.createElement('img');
-    media.setAttribute('src', entry.media);
+    media.setAttribute('src', `media/${entry.media}`);
     media.setAttribute('alt', entry.name);
     entryDiv.appendChild(media);
   }
