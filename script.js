d692bbc629a12e18fb1c92c4c53aed3ac6a0bda9
SploeCyber <champgamerthai@gmail.com>
2023-08-06 14:57:50 +0700

    asdadwdaw

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// scripts.js
 (async function () {
   const tabsContainer = document.getElementById('tabsContainer');
 
   async function fetchData() {
     try {
       const response = await fetch('entries.json');
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
       return await response.json();
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -18,66 +22,19 @@
     createTabs(uniqueTiers);
 
     entries.forEach(entry => {
-      const entryDiv = document.createElement('div');
-      entryDiv.classList.add('tab-content');
-      entryDiv.dataset.tier = entry.tier || 'N/A';
-
-      const title = document.createElement('h2');
-      title.textContent = entry.name;
-      entryDiv.appendChild(title);
-
-      if (entry.description) {
-        const description = document.createElement('p');
-        description.textContent = entry.description;
-        entryDiv.appendChild(description);
-      }
-
-      if (entry.media && entry.mediaType === 'image' && entry.media !== 'none') {
-        const media = document.createElement('img');
-        media.setAttribute('src', `media/${entry.media}`);
-        media.setAttribute('alt', entry.name);
-        entryDiv.appendChild(media);
-      }
-
-      const tierTag = document.createElement('p');
-      tierTag.textContent = `Tier: ${entry.tier || 'N/A'}`;
-      entryDiv.appendChild(tierTag);
-
-      const tags = document.createElement('p');
-      tags.classList.add('tags');
-      if (entry.tags) {
-        tags.innerHTML = `Tags: ${entry.tags.map(tag => `<span class="tag" data-tag="${tag}">${tag}</span>`).join(', ')}`;
-      } else {
-        tags.textContent = 'Tags: N/A';
-      }
-      entryDiv.appendChild(tags);
-
+      const entryDiv = createEntryDiv(entry);
       const tierTab = document.getElementById(`tier${entry.tier}`);
       if (tierTab) {
         tierTab.appendChild(entryDiv);
       }
     });
 
-    const tabLinks = document.querySelectorAll('.tab-link');
-    tabLinks.forEach(tabLink => {
-      tabLink.addEventListener('click', () => {
-        const tabName = tabLink.getAttribute('data-tab');
-        switchTab(tabName);
-      });
-    });
-
-    const tagElements = document.querySelectorAll('.tag');
-    tagElements.forEach(tagElement => {
-      tagElement.addEventListener('click', () => {
-        const selectedTag = tagElement.getAttribute('data-tag');
-        filterByTag(selectedTag);
-      });
-    });
+    addTabEventListeners();
+    addTagEventListeners();
   }
 
   function findUniqueTiers(entries) {
-    const uniqueTiers = new Set();
-    entries.forEach(entry => uniqueTiers.add(entry.tier));
+    const uniqueTiers = new Set(entries.map(entry => entry.tier).filter(Boolean));
     return Array.from(uniqueTiers).sort();
   }
 
@@ -93,6 +50,64 @@
       tabContent.classList.add('tab');
       tabContent.setAttribute('id', `tier${tier}`);
       tabsContainer.appendChild(tabContent);
+    });
+  }
+
+  function createEntryDiv(entry) {
+    const entryDiv = document.createElement('div');
+    entryDiv.classList.add('tab-content');
+    entryDiv.dataset.tier = entry.tier || 'N/A';
+
+    const title = document.createElement('h2');
+    title.textContent = entry.name;
+    entryDiv.appendChild(title);
+
+    if (entry.description) {
+      const description = document.createElement('p');
+      description.textContent = entry.description;
+      entryDiv.appendChild(description);
+    }
+
+    if (entry.media && entry.mediaType === 'image' && entry.media !== 'none') {
+      const media = document.createElement('img');
+      media.setAttribute('src', `media/${entry.media}`);
+      media.setAttribute('alt', entry.name);
+      entryDiv.appendChild(media);
+    }
+
+    const tierTag = document.createElement('p');
+    tierTag.textContent = `Tier: ${entry.tier || 'N/A'}`;
+    entryDiv.appendChild(tierTag);
+
+    const tags = document.createElement('p');
+    tags.classList.add('tags');
+    if (entry.tags) {
+      tags.innerHTML = `Tags: ${entry.tags.map(tag => `<span class="tag" data-tag="${tag}">${tag}</span>`).join(', ')}`;
+    } else {
+      tags.textContent = 'Tags: N/A';
+    }
+    entryDiv.appendChild(tags);
+
+    return entryDiv;
+  }
+
+  function addTabEventListeners() {
+    const tabLinks = document.querySelectorAll('.tab-link');
+    tabLinks.forEach(tabLink => {
+      tabLink.addEventListener('click', () => {
+        const tabName = tabLink.getAttribute('data-tab');
+        switchTab(tabName);
+      });
+    });
+  }
+
+  function addTagEventListeners() {
+    const tagElements = document.querySelectorAll('.tag');
+    tagElements.forEach(tagElement => {
+      tagElement.addEventListener('click', () => {
+        const selectedTag = tagElement.getAttribute('data-tag');
+        filterByTag(selectedTag);
+      });
     });
   }
 
@@ -124,6 +139,10 @@
     });
   }
 
-  const data = await fetchData();
-  displayData(data);
+  try {
+    const data = await fetchData();
+    displayData(data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 })();
